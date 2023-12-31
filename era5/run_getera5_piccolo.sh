04d3225ca1d2f97be2037411c9e3330b6889c0e9
jhruppert <jruppert.jr@gmail.com>
2023-12-31 11:23:53 -0700

    updates

---
+++ era5/run_getera5_piccolo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+
+DATE1=20230812
+DATE2=20230814
+Nort=20
+West=-40
+Sout=-5
+East=-10
+
+
+YY1=`echo $DATE1 | cut -c1-4`
+MM1=`echo $DATE1 | cut -c5-6`
+DD1=`echo $DATE1 | cut -c7-8`
+YY2=`echo $DATE2 | cut -c1-4`
+MM2=`echo $DATE2 | cut -c5-6`
+DD2=`echo $DATE2 | cut -c7-8`
+
+
+sed -e "s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;s/Nort/${Nort}/g;s/West/${West}/g;s/Sout/${Sout}/g;s/East/${East}/g;" GetERA5-sl.py > GetERA5-${DATE1}-${DATE2}-sl.py
+
+
+python GetERA5-${DATE1}-${DATE2}-sl.py
+
+
+sed -e "s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;s/Nort/${Nort}/g;s/West/${West}/g;s/Sout/${Sout}/g;s/East/${East}/g;" GetERA5-pl.py > GetERA5-${DATE1}-${DATE2}-pl.py
+
+
+python GetERA5-${DATE1}-${DATE2}-pl.py
+
